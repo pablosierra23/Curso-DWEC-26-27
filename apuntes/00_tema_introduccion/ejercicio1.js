1f934267b7852f2cf931bea8122bce290295eaf8
pablosierra23 <pablosg1008@gmail.com>
2026-09-18 11:17:11 +0200

    Primer dia

---
+++ apuntes/00_tema_introduccion/ejercicio1.js
@@ -0,0 +1,49 @@
+/*
+
+//Ejercicio 1 de JavaScriptj
+//
+
+
+console.log("Hola mundo")
+
+//Tipos de datos en JavaScript
+//
+// String y number
+// '' "" ` comillas francesas `
+// var let const
+let nombre = "Pablo"
+let apellidos = "SG"
+let aniosTrabajo = 25
+console.log(`Hola a todos me llamo ${nombre}, ${apellidos} y llevo trabajando ${aniosTrabajo} años`)
+console.log(typeoff(String(aniosTrabajo))
+console.log(typeoff(Number(apellidos)))
+
+//Validaciones basicas == ===
+
+// == <-- significa si el alor de la izquierda es igual al valor de la derecha
+// === <-- significa si el valor y tipo de la izquierda coincide con el valor y tipo de la derecha
+
+//'5' === 5 // <-- false
+//'5' == 5 // <-- true
+
+//ternarias evaluacion_expresion ? verdadero : falso
+//
+
+
+const edad = "23"
+edad > 18 ? console.log("Eres mayor de edad") : console.log("Eres menor de edad")
+
+*/
+
+//Ejercicio : Dada la edad, los minutos y los segundos. Comprobar si la edad es un numero positivo 
+//y mayor que 18. Comprobar si la hora y los minutos son valores validos dentro de nuestro 
+//sistema de numeracion.
+
+const edad = 19
+const hora = 2
+const minutos = 30
+const segundos = 25
+
+edad >= 0 && edad > 18 ? console.log("El numero es positivo y mayor que 18") : console.log("El numero es negativo y menor que 18")
+
+
