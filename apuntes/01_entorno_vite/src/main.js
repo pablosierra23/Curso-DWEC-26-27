80f15c5d706d9eab2e66d0eef2e189a37f6dd889
pablosierra23 <pablosg1008@gmail.com>
2026-09-22 10:18:36 +0200

    Ejercicios dia 2

---
+++ apuntes/01_entorno_vite/src/main.js
@@ -0,0 +1,15 @@
+
+document.querySelector('#app').innerHTML = `
+<section id="center">
+<h1>Hola Mundo</h1>
+</section>
+
+<div class="ticks"></div>
+
+<section id="next-steps">
+
+  </section>
+
+<div class="ticks"></div>
+<section id="spacer"></section>
+`
---
+++ apuntes/01_entorno_vite/src/tipo_datos.js
@@ -0,0 +1,47 @@
+// Funcion que le pase como parametro un numero en grados celsius y lo 
+// transforme a grados kelvin
+
+//  V1 (casi nunca usaremos algo tan verboso)
+
+function celsiusToKelvin(celsius){
+  let kelvin = celsius + 273.15
+  return kelvin
+}
+
+
+// V2 (version donde preorizamos el menor numero de lineas)
+
+function celsiusToKelvin(celsius){
+  return celsius + 273.15
+}
+
+
+//V3 (modo pro usando arrow function)
+
+const celToKel = (celsius) => {
+  return celsius + 273.15
+} 
+
+
+//V4 (modo dios pro max)
+
+const cToK = ( c ) => c + 273.15
+
+//Funcion que le pase como parametro dos numeros y me los ordene
+
+
+
+//Funcion que pase de celsius a kelvin pero comprobando que celsius es un numero, 
+//que la temperatura no puede estar por debajo del 0 absoluto (-273  ºK)
+//y el resultado me lo das con solo dos cifras decimales
+//isNaN <---- buscamos lo que significa
+// ¿como truncamos un numero a 2 cifras decimales?
+
+if(xxxx){
+
+}else {
+
+}
+
+
+
